b15c3d0df18bab1002b7aae214d9a2cf86d65758
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-17 17:08:29 +0300

    add slice

--- slice4.rs
+++ slice4.rs
@@ -1,7 +1,12 @@
 fn main() {
     let ints = [1, 2, 3, 4, 5];
     let slice = &ints;
-    for i in [1, 2, 3, 4].iter() {
-        println!("{}", i);
+
+    for s in slice.windows(2) {
+        println!("window {:?}", s);
+    }
+
+    for s in slice.chunks(2) {
+        println!("chuncks {:?}", s);
     }
 }
