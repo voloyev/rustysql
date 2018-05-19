5d9c5ce51b4e0938252bcd437f672ce977ca925b
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-05-19 15:21:54 +0300

    Add simple tokenizer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 mod tokenizer;
+
 fn main() {
     let mut db = HashMap::new();
     repl(&mut db);
@@ -22,10 +23,11 @@ fn repl(db: &mut HashMap<i32, String>) {
         let (input, write) = check_input(tmp, db);
 
         if write {
-            println!("{}", input);
-        } else {
             counter += 1;
+            println!("{:?}", tokenizer::run(&input));
             db.insert(counter, input);
+        } else {
+            println!("{}", input);
         }
     }
 }
@@ -41,17 +43,19 @@ fn read_line() -> String {
 fn check_input(input: String, db: &mut HashMap<i32, String>) -> (String, bool) {
     if input.trim() == ".help" {
         return (
-            String::from("Available commands are: \n\n.exit\tto exit program"),
-            true,
+            String::from(
+                "Available commands are: \n\n.exit\tto exit program\n.help\tto show this help\n",
+            ),
+            false,
         );
     }
 
     if input.trim() == ".select" {
         println!("{:?}", db);
-        return (String::from(""), true);
+        return (String::from(""), false);
     }
 
-    return (input, false);
+    return (input, true);
 }
 
 #[test]
@@ -61,8 +65,10 @@ fn check_help_input_test() {
     assert_eq!(
         a,
         (
-            String::from("Available commands are: \n\n.exit\tto exit program"),
-            true
+            String::from(
+                "Available commands are: \n\n.exit\tto exit program\n.help\tto show this help\n"
+            ),
+            false
         )
     )
 }
@@ -71,5 +77,5 @@ fn check_help_input_test() {
 fn check_select_input_test() {
     let mut db = HashMap::new();
     let a = check_input(String::from(".select"), &mut db);
-    assert_eq!(a, (String::from(""), true))
+    assert_eq!(a, (String::from(""), false))
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,7 +1,14 @@
-pub fn tokenize(query: &String) -> Vec<&str> {
+pub fn run(query: &String) -> Vec<&str> {
     analize(query)
 }
 
 fn analize(query: &String) -> Vec<&str> {
     return query.as_str().split(" ").collect();
+}
+
+#[test]
+fn test_analyze() {
+    let some_str = String::from("select 1 from db");
+    let result = analize(&some_str);
+    assert_eq!(result, vec!["select", "1", "from", "db"]);
 }
