4a620385cd2c50506da9ae9dc05a020a2b5205fd
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-06-10 12:48:04 +0300

    Add whitespace

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ fn repl(db: &mut HashMap<i32, String>) {
 fn read_line() -> String {
     print!("db_name > ");
     io::stdout().flush().unwrap();
+
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("somthing wrong");
     input
